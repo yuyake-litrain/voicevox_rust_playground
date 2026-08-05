bbf45c5b96d19ae4ae4966b46b0aa517b85dfc97
yuyake-litrain <85449119+yuyake-litrain@users.noreply.github.com>
2026-08-05 11:45:06 +0900

    Add: output libs path (following example)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use const_format::concatcp;
+use voicevox_core::blocking::{Onnxruntime};
+
 fn main() {
-    println!("Hello, world!");
+    const PATH_TO_DYLIB: &str = concatcp!(
+        "./voicevox_core/onnxruntime/lib/",
+        Onnxruntime::LIB_VERSIONED_FILENAME
+        );
+    println!("{PATH_TO_DYLIB}",)
 }
