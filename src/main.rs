9cc0f61ab663cd693ab9cd510663f5d955ca2976
yuyake-litrain <85449119+yuyake-litrain@users.noreply.github.com>
2026-09-12 03:11:33 +0900

    Feat: GUI for tts

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
+use std::rc::Rc;
 use std::{env, fs::File, io::Write};
 
+use iced::Theme;
+use iced::widget::{Column, button, column, text, text_input};
+use voicevox_core::StyleId;
 use voicevox_core::{
     CharacterMeta, StyleMeta,
     blocking::{Onnxruntime, OpenJtalk, Synthesizer, VoiceModelFile},
 };
 
-fn main() {
+use crate::Message::TTSBtnPressed;
+
+const APP_NAME: &str = "VOICEVOX Rust GUI";
+
+fn main() -> iced::Result {
     let current_exe_tree = |path: &str| {
         format!(
             "{}{}",
@@ -54,9 +62,70 @@ fn main() {
         .find(|StyleMeta { name, .. }| name == "ノーマル")
         .unwrap();
 
-    let wav = &synth.tts("こんにちは", style_id).perform().unwrap();
+    // let wav = &synth.tts("こんにちは", style_id).perform().unwrap();
 
-    let mut file = File::create("zuda.wav").unwrap();
-    file.write_all(wav).unwrap();
+    // let mut file = File::create("zuda.wav").unwrap();
+    // file.write_all(wav).unwrap();
     // dbg!(wav);
+
+    let synth = Rc::new(synth);
+
+    iced::application(
+        move || IcedVVGUIState::new(synth.clone(), style_id),
+        update,
+        view,
+    )
+    .theme(Theme::Dark)
+    .title(APP_NAME)
+    .run()
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    TextEdited(String),
+    TTSBtnPressed,
+}
+
+struct IcedVVGUIState {
+    synth: Rc<Synthesizer<OpenJtalk>>,
+    current_text: String,
+    style_id: StyleId,
+}
+
+impl IcedVVGUIState {
+    fn new(synth: Rc<Synthesizer<OpenJtalk>>, style_id: StyleId) -> Self {
+        Self {
+            synth,
+            current_text: String::default(),
+            style_id,
+        }
+    }
+}
+
+fn update(state: &mut IcedVVGUIState, message: Message) {
+    match message {
+        Message::TextEdited(text) => {
+            state.current_text = text;
+        }
+        TTSBtnPressed => {
+            let wav = &state
+                .synth
+                .tts(&state.current_text, state.style_id)
+                .perform()
+                .unwrap();
+            let mut file = File::create(format!("zunda_{}.wav", state.current_text)).unwrap();
+            file.write_all(wav).unwrap();
+        }
+    }
+}
+
+fn view<'a>(state: &'a IcedVVGUIState) -> Column<'a, Message> {
+    column![
+        text(format!(
+            "以下の文字列が音声合成され保存されます\n{}",
+            &state.current_text
+        )),
+        text_input("Input text...", &state.current_text).on_input(Message::TextEdited),
+        button("TTS!").on_press(Message::TTSBtnPressed)
+    ]
 }
