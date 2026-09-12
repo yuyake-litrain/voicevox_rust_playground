7b99bd1ab2967e47abcd3b1ec949b2d4b0fe898d
yuyake-litrain <85449119+yuyake-litrain@users.noreply.github.com>
2026-09-12 23:17:18 +0900

    Feat: menubar and styling

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,14 @@ use std::io::Cursor;
 use std::sync::Arc;
 use std::{env, fs::File, io::Write};
 
-use iced::widget::{Column, button, column, combo_box, row, text, text_input};
-use iced::{Alignment, Theme};
+use iced::widget::{
+    Container, button, column, combo_box, container,
+    pane_grid,
+    row, text, text_input,
+};
+use iced::{Alignment, Background, Border, Color, Length, Theme, color};
+use iced_aw::style::menu_bar;
+use iced_aw::widget::menu::{Item, Menu, MenuBar};
 use voicevox_core::StyleId;
 use voicevox_core::{
     CharacterMeta, StyleMeta,
@@ -117,35 +123,123 @@ impl IcedVVGUIState {
         }
     }
 
-    fn view<'a>(state: &'a IcedVVGUIState) -> Column<'a, Message> {
-        column![
-            text(format!(
-                "以下の文字列が音声合成され保存されます\n{}",
-                &state.current_text
-            )),
-            row![
-                column![
-                    combo_box(
-                        &state.characters,
-                        "Select Characters",
-                        state.current_character.as_ref(),
-                        Message::CharacterSelected
-                    ),
-                    combo_box(
-                        &state.current_character_styles,
-                        "Select Characters",
-                        state.current_character_style.as_ref(),
-                        Message::CharacterStyleSelected,
-                    )
+
+    fn view<'a>(state: &'a IcedVVGUIState) -> Container<'a, Message> {
+        let style = |_: &Theme, _: button::Status| button::Style {
+            background: None,
+            text_color: Color::WHITE,
+            ..Default::default()
+        };
+        container(
+            column![
+                // メニューバー
+                row!({
+                    let root_file = Item::with_menu(
+                        button("File").style(style),
+                        Menu::new(vec![
+                            Item::new(button("Open Project").style(style)),
+                            Item::new(button("Close").style(style)),
+                            Item::with_menu(
+                                button("Recent Projects").style(style),
+                                Menu::new(vec![
+                                    Item::new(button("hoge_prj1").style(style)),
+                                    Item::new(button("hoge_prj2").style(style)),
+                                    Item::new(button("hoge_prj3").style(style)),
+                                ]),
+                            ),
+                            Item::new(button("Save").style(style)),
+                            Item::new(button("Save as...").style(style)),
+                            Item::new(button("Exit").style(style)),
+                        ]),
+                    );
+
+                    let root_edit = Item::with_menu(
+                        button("Edit").style(style),
+                        Menu::new(vec![
+                            Item::new(button("Cut").style(style)),
+                            Item::new(button("Copy").style(style)),
+                            Item::new(button("Paste").style(style)),
+                            Item::new(button("Select All").style(style)),
+                            Item::new(button("Remove").style(style)),
+                            Item::new(button("Settings...").style(style)),
+                        ]),
+                    );
+
+                    let root_view = Item::with_menu(
+                        button("View").style(style),
+                        Menu::new(vec![Item::new(button("Set default").style(style))]),
+                    );
+                    let root_engine = Item::with_menu(
+                        button("Engine").style(style),
+                        Menu::new(vec![
+                            Item::new(button("Engines...").style(style)),
+                            Item::new(button("Models...").style(style)),
+                        ]),
+                    );
+                    let root_plugins = Item::with_menu(
+                        button("Plugins").style(style),
+                        Menu::new(vec![Item::new(button("Not Implemented").style(style))]),
+                    );
+                    MenuBar::new(vec![
+                        root_file,
+                        root_edit,
+                        root_view,
+                        root_engine,
+                        root_plugins,
+                    ])
+                    .style(|_, _| menu_bar::Style {
+                        menu_border: Border {
+                            color: color!(0x444444),
+                            width: 1.0,
+                            ..Default::default()
+                        },
+                        bar_background: Background::Color(Color::TRANSPARENT),
+                        menu_background: Background::Color(color!(0x0F0F0F)),
+                        ..Default::default()
+                    })
+                },)
+                .width(Length::Fill)
+                .align_y(Alignment::Start),
+                // 線
+                container("")
+                    .width(Length::Fill)
+                    .height(2.0)
+                    .style(|_| container::Style {
+                        background: Some(Background::Color(color!(0x444444))),
+                        ..Default::default()
+                    }),
+                text(format!(
+                    "以下の文字列が音声合成され保存されます\n{}",
+                    &state.current_text
+                )),
+                row![
+                    column![
+                        combo_box(
+                            &state.characters,
+                            "Select Characters",
+                            state.current_character.as_ref(),
+                            Message::CharacterSelected
+                        ),
+                        combo_box(
+                            &state.current_character_styles,
+                            "Select Characters",
+                            state.current_character_style.as_ref(),
+                            Message::CharacterStyleSelected,
+                        )
+                    ],
+                    text_input("Input text...", &state.current_text).on_input(Message::TextEdited),
                 ],
-                text_input("Input text...", &state.current_text).on_input(Message::TextEdited),
-            ],
-            row![
-                button("TTS & Save!").on_press(Message::TTSBtnPressed),
-                button("Say").on_press(Message::SayBtnPressed)
+                row![
+                    button("TTS & Save!").on_press(Message::TTSBtnPressed),
+                    button("Say").on_press(Message::SayBtnPressed)
+                ]
             ]
-        ]
-        .align_x(Alignment::Center)
+            .align_x(Alignment::Center),
+        )
+        .style(|_| container::Style {
+            background: Some(Background::Color(color!(0x0F0F0F))),
+            ..Default::default()
+        }).height(Length::Fill)
     }
 }
 
